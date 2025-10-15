901674fdcd79719f90f58046c4c42cf72e4d7a98
Ariel Buchholz <a.81.buchholz@gmail.com>
2025-10-14 23:22:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,6 @@ import streamlit as st
 
 st.title("PRACTICA DE STREAMLIT")
 st.write("Este es un programa web de prueba para mostrar como subir tu app desde el repositorio a Streamlit Cloud para la Web")
+
+st.subheader("Practica de Hoy")
+st.write("Hoy volvimos a repasar como subir una pagina web a la nube y repasamos trabajos de Scudero")
