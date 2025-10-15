a93f4f95815ca2e42689789cc596d74001331bf0
Ariel Buchholz <a.81.buchholz@gmail.com>
2025-10-14 23:24:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,5 @@ st.write("Este es un programa web de prueba para mostrar como subir tu app desde
 
 st.subheader("Practica de Hoy")
 st.write("Hoy volvimos a repasar como subir una pagina web a la nube y repasamos trabajos de Scudero")
+
+st.badge("Mensaje Nuevo")
